33ed50a861c2b960ef4eb0ba3fdd73bc2dc4d1b4
edroga <ilan2884@hotmail.com>
2019-05-29 03:14:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,22 +5,22 @@ A = matrix([
     np.ones(d, float).tolist(), r_i.tolist()
     ]).trans()
 b = matrix([1.0, float(rho)])
-    ##########################################################
-    # Se define la restricción para cuando las ventas en corto
-    # están permitidas
-    ##########################################################
-    diagonal = np.zeros((d, d), float)
-    np.fill_diagonal(diagonal, -1.0)
+##########################################################
+# Se define la restricción para cuando las ventas en corto
+# están permitidas
+##########################################################
+diagonal = np.zeros((d, d), float)
+np.fill_diagonal(diagonal, -1.0)
 
-    G = matrix(diagonal)
-    h = matrix(np.zeros(d, float))
-    ##########################################################
-    # Se revisa la condición de Ventas en corto para asignar
-    # valores a G y h
-    ##########################################################
-    if Ventas_en_corto == True:
-        G = None
-        h = None
-    else:
-        G = G
-        h = h
+G = matrix(diagonal)
+h = matrix(np.zeros(d, float))
+##########################################################
+# Se revisa la condición de Ventas en corto para asignar
+# valores a G y h
+##########################################################
+if Ventas_en_corto == True:
+    G = None
+    h = None
+else:
+    G = G
+    h = h
